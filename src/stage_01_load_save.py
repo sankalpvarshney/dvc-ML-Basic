a4d9f93f3c0c914d15a3845e4dc16cbcd68c0004
Sankalp varshney <sankalp.varshney@biocube.ai>
2022-08-31 00:45:23 +0530

    stage 01 created

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,37 @@
+from ast import arg
+from curses import raw
+import imp
+import re
+from src.utils.all_utils import read_yaml,create_directory
+import argparse
+import pandas as pd
+import os
+
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    remote_data_path = config["data_source"]
+    df = pd.read_csv(remote_data_path,sep=";")
+
+    # Save the dataset in the local directory
+    # Create path to directory: artifact/raw_local_dir/data.csv
+    artifact_dir = config["artifacts"]["artifacts_dir"]
+    raw_local_dir = config["artifacts"]["raw_local_dir"]
+    raw_local_file = config["artifacts"]["raw_local_file"]
+
+    raw_local_dir_path = os.path.join(artifact_dir,raw_local_dir)
+    raw_local_file_path = os.path.join(raw_local_dir_path,raw_local_file)
+    create_directory(dirs=[raw_local_dir_path])
+    
+    df.to_csv(raw_local_file_path,sep=",",index=False)
+    # print(raw_local_file_path)
+
+    # print(df.head())
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="config/config.yaml")
+    
+    parsed_args = args.parse_args()
+    get_data(config_path=parsed_args.config)
+    # print(parsed_args.config)
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -6,3 +6,8 @@ def read_yaml(path_to_yaml: str) -> dict:
         content = yaml.safe_load(yaml_file)
     
     return content
+
+def create_directory(dirs: list):
+    for dir_parh in dirs:
+        os.makedirs(dir_parh, exist_ok=True)
+        print(f"directory is created at {dir_parh}")
