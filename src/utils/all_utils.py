f738e94bfe6774f9ee40d79178757351f64c28cc
Sankalp varshney <sankalp.varshney@biocube.ai>
2022-09-01 23:16:49 +0530

    stage 02 added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -11,3 +11,8 @@ def create_directory(dirs: list):
     for dir_parh in dirs:
         os.makedirs(dir_parh, exist_ok=True)
         print(f"directory is created at {dir_parh}")
+
+def save_local_df(data,data_path,index_status=False):
+    data.to_csv(data_path, index=index_status)
+    print(f"Data is saved at the path {data_path}")
+
